333f34e1e6003307c817b03efd0566a251bbe4aa
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-03 23:36:31 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -2,5 +2,9 @@
 import { applyCors } from '../utils/cors.js';
 
 export default function handler(req, res) {
+  applyCors(res);
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   res.status(200).json({ status: 'ok' });
 }
