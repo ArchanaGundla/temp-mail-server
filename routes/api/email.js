c6a65ce5a2a218c849bc7dbc2c46de0feb6859a8
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-03 21:49:56 +0530

    Delete routes directory

--- routes/api/email.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({ message: "Hello from backend" });
-  } else {
-    res.status(405).end("Method not allowed");
-  }
-}
