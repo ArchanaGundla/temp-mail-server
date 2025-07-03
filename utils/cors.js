2d5bf93984580d0711756f51f542454cbc59eab0
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-03 23:40:04 +0530

    Update cors.js

--- utils/cors.js
+++ utils/cors.js
@@ -1,5 +1,10 @@
+// utils/cors.js
+
 export function applyCors(res) {
+  // Allow any origin (you can lock this down to your frontend domain if you like)
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // Allow these methods
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  // Allow these headers in requests
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
