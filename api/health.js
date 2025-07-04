5ff87ab7bbc2b33306a9d79c094e7d48395dc036
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-04 22:49:28 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,13 +1,9 @@
 import cors from '../../lib/cors-middleware';
 
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "https://temp-mail-olive-ten.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
