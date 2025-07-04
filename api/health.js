0f3c89ab155dab51a949a0299b1d2dba1e082e83
ArchanaGundla <86965852+ArchanaGundla@users.noreply.github.com>
2025-07-04 22:51:22 +0530

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,8 +1,6 @@
-import cors from '../../lib/cors-middleware';
-
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "https://temp-mail-olive-ten.vercel.app");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
